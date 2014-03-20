779bf7411a0c2b719e9251c314b38a86e3160982
Arthur Leonard Andersen <leoc.git@gmail.com>
2014-03-20 23:31:10 +0100

    Clean up setup-ido.el

--- setup/setup-ido.el
+++ setup/setup-ido.el
@@ -1,4 +1,22 @@
+;;; setup-ido.el --- Setup ido mode.
+;;; Commentary:
+;;; Code:
+(ensure-packages '(ido-ubiquitous
+                   ido-vertical-mode
+                   flx-ido))
+
 (ido-mode t)
+
+;; Use ido everywhere
+(require 'ido-ubiquitous)
+(ido-ubiquitous-mode t)
+
+;; Enable flx-ido
+(require 'flx-ido)
+(flx-ido-mode t)
+;; Disable ido faces to see flx highlights.
+(setq ido-use-faces nil)
+
 (setq ido-enable-prefix nil
       ido-enable-flex-matching t
       ido-case-fold nil
@@ -7,20 +25,15 @@
       ido-use-filename-at-point nil
       ido-max-prospects 10)
 
-(add-hook
- 'ido-setup-hook
- (lambda ()
-     ;; Use C-w to go back up a dir to better match normal usage of C-w
-     ;; - insert current file name with C-x C-w instead.
-     (define-key ido-file-completion-map (kbd "C-w") 'ido-delete-backward-updir)
-     (define-key ido-file-completion-map (kbd "C-x C-w") 'ido-copy-current-file-name)))
+(add-hook 'ido-setup-hook
+          (lambda ()
+            ;; Use C-w to go back up a dir to better match normal usage of C-w
+            ;; - insert current file name with C-x C-w instead.
+            (define-key ido-file-completion-map (kbd "C-w") 'ido-delete-backward-updir)
+            (define-key ido-file-completion-map (kbd "C-x C-w") 'ido-copy-current-file-name)))
 
 ;; Always rescan buffer for imenu
 (set-default 'imenu-auto-rescan t)
-
-;; Use ido everywhere
-(require 'ido-ubiquitous)
-(ido-ubiquitous-mode 1)
 
 ;; Fix ido-ubiquitous for newer packages
 (defmacro ido-ubiquitous-use-new-completing-read (cmd package)
@@ -32,6 +45,8 @@
 (ido-ubiquitous-use-new-completing-read yas/expand 'yasnippet)
 (ido-ubiquitous-use-new-completing-read yas/visit-snippet-file 'yasnippet)
 
+;; Vertical mode is much more friendly to the eye...
 (ido-vertical-mode)
 
 (provide 'setup-ido)
+;;; setup-ido.el ends here
