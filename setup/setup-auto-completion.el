80b1486fd8afa7badc8ce1d48d1abb6194dad778
Arthur Leonard Andersen <leoc.git@gmail.com>
2014-04-15 22:17:53 +0200

    Customize completions colors

--- setup/setup-auto-completion.el
+++ setup/setup-auto-completion.el
@@ -13,6 +13,16 @@
 (define-key company-active-map (kbd "TAB") 'company-complete-selection)
 (define-key company-active-map (kbd "C-j") 'company-complete-selection)
 
+(require 'color)
+
+(let ((bg (face-attribute 'default :background)))
+  (custom-set-faces
+   `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+   `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+   `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+   `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+   `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
+
 (provide 'setup-auto-completion)
 
 ;;; setup-auto-completion.el ends here
