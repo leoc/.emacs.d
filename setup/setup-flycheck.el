b91cef27935c40722f9eac4a1ce2e3bc81f358d2
Arthur Leonard Andersen <leoc.git@gmail.com>
2014-05-05 09:56:02 +0200

    Add keybindings for cycling through errors

--- setup/setup-flycheck.el
+++ setup/setup-flycheck.el
@@ -1,5 +1,7 @@
 (add-hook 'prog-mode-hook 'flycheck-mode)
 
-(ensure-package 'flycheck-tip)
+(ensure-package-and-require 'flycheck-tip)
+(global-set-key [up] 'flycheck-tip-cycle-reverse)
+(global-set-key [down] 'flycheck-tip-cycle)
 
 (provide 'setup-flycheck)
