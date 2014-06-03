81b6cb9a88fd19c952581b316dd5ce7cd7860002
Arthur Leonard Andersen <leoc.git@gmail.com>
2014-06-03 12:53:19 +0200

    Move faces into summered theme

--- setup/setup-appearance.el
+++ setup/setup-appearance.el
@@ -3,17 +3,6 @@
       color-theme-is-global t
       truncate-partial-width-windows nil)
 
-;; Recolor the region background
-(set-face-background 'region "#464740")
-
-;; Highlight current line
-(global-hl-line-mode 1)
-
-;; Customize background color of lighlighted line
-(set-face-background 'hl-line "#222222")
-
-(set-face-foreground 'font-lock-warning-face "#ff6666")
-
 ;; Highlight matching parentheses when the point is on them.
 (show-paren-mode 1)
 
