1f53a423dac6c9f0cb3d699e0bd619c026493cb2
Arthur Andersen <leoc.git@gmail.com>
2015-09-23 17:31:23 +0200

    Configure backup and auto-safe directories

--- setup/setup-sane-defaults.el
+++ setup/setup-sane-defaults.el
@@ -3,8 +3,26 @@
 ;;; code:
 
 ;; Write backup files to own directory
-(setq backup-directory-alist `((".*" . ,tmp-dir)))
-(setq auto-save-file-name-transforms `((".*" ,tmp-dir t)))
+(setq backup-directory-alist `((".*" . ,backups-dir)))
+(unless (file-exists-p backups-dir)
+  (make-directory backups-dir t))
+(setq make-backup-files t               ; backup of a file the first time it is saved.
+      backup-by-copying t               ; don't clobber symlinks
+      version-control t                 ; version numbers for backup files
+      delete-old-versions t             ; delete excess backup files silently
+      delete-by-moving-to-trash t
+      kept-old-versions 6               ; oldest versions to keep when a new numbered backup is made (default: 2)
+      kept-new-versions 9               ; newest versions to keep when a new numbered backup is made (default: 2)
+      auto-save-default t               ; auto-save every buffer that visits a file
+      auto-save-timeout 20              ; number of seconds idle time before auto-save (default: 30)
+      auto-save-interval 200            ; number of keystrokes between auto-saves (default: 300)
+      )
+
+;; Write auto-save files to custom directory
+(unless (file-exists-p auto-saves-dir)
+  (make-directory auto-saves-dir t))
+(setq auto-save-file-name-transforms `((".*" ,auto-saves-dir t)))
+
 ;; Change the temporary file directory
 (unless (file-exists-p tmp-dir)
   (make-directory tmp-dir t))
