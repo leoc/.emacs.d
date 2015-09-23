6932dceed609eef3af5c8beb83e3536ac46a3ff6
Arthur Andersen <leoc.git@gmail.com>
2015-09-23 17:31:38 +0200

    Load rubocop with ruby-mode

--- setup/setup-ruby.el
+++ setup/setup-ruby.el
@@ -4,13 +4,15 @@
                    ruby-block
                    ruby-end
                    ruby-tools
+                   rubocop
                    rvm))
 
 (defun custom-ruby-hook ()
   "Define a custom ruby mode hook."
   (robe-mode)
   (push 'company-robe company-backends)
-  (set (make-local-variable 'ac-delay) 2))
+  (set (make-local-variable 'ac-delay) 2)
+  (rubocop-mode))
 
 (add-hook 'ruby-mode-hook 'custom-ruby-hook)
 
