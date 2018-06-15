ef202db97a45d6a61d9aec49cfbead760bcfa5a8
Arthur Leonard Andersen <aandersen@velalu.qa>
2018-06-15 22:33:31 +0200

    Remove first `(package-initialize)`

--- init.el
+++ init.el
@@ -3,8 +3,6 @@
 ;; This file loads Org-mode and then loads the rest of our Emacs
 ;; initialization from Emacs lisp embedded in literate Org-mode files.
 
-(package-initialize)
-
 (let ((org-confirm-babel-evaluate nil))
   (org-babel-load-file "/home/arthur/.emacs.d/init-emacs.org")) 
 
